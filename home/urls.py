cc91d7b8653984e37fe4dbfc7aba3b2749df6f57
Gbemi <gbemi@Gs-MacBook-Pro.local>
2020-10-03 21:02:49 -0400

    upload page added

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+# from django.urls import path
 
-from . import views
+# from . import views
 
-urlpatterns = [
-    path('', views.home, name='home'),
-    # path('admin/', admin.site.urls),
+# urlpatterns = [
+#     path('', views.home, name='home'),
+#     # path('admin/', admin.site.urls),
 
-]
+# ]
+
